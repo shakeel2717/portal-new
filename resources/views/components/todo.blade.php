875dcd5aab151ced7c9878a70665b54a273de378
Shakeel Ahmad <shakeel2717@gmail.com>
2022-07-30 02:31:43 -0700

    static dasbhoard update

---
+++ resources/views/components/todo.blade.php
@@ -0,0 +1,41 @@
+<div class="card border-top border-primary">
+  <div class="card-body">
+    <div class="d-flex justify-content-start">
+      <a href="#" class="btn btn-primary btn-sm mx-2">High</a>
+      <a href="#" class="btn btn-warning btn-sm  mx-2">Repairing</a>
+    </div>
+    <div class="mt-4">
+      <h5 class="card-title">Repair Macbook-121233</h5>
+    </div>
+    <div class="mt-4">
+      <div class="employees d-flex">
+        <a href="#">
+          <div class="iq-media-group iq-media-group-1">
+            <img src="{{ asset('assets/brand/profile-dark.svg') }}" alt="" class="avatar-50">
+          </div>
+        </a>
+        <a href="#">
+          <div class="iq-media-group iq-media-group-1" style="margin-left:-20px ">
+            <img src="{{ asset('assets/brand/profile-dark.svg') }}" alt="" class="avatar-50">
+          </div>
+        </a>
+      </div>
+    </div>
+    <div class="footer mt-4 mb-0">
+      <div class="d-flex justify-content-between">
+        <div class="comments fs-5 d-flex">
+          <i class="bi bi-chat-left"></i>
+          <div class="value ms-1">0</div>
+        </div>
+        <div class="attatchment fs-5 d-flex">
+          <i class="bi bi-paperclip"></i>
+          <div class="value ms-1">0</div>
+        </div>
+        <div class="delay fs-5 d-flex">
+          <i class="bi bi-stopwatch"></i>
+          <div class="value ms-1">Due 10 Nov 2021</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- resources/views/employee/dashboard/index.blade.php
+++ resources/views/employee/dashboard/index.blade.php
@@ -1,1 +1,55 @@
-@extends("layouts.dashboard")
+@extends('layouts.dashboard')
+@section('content')
+    <div class="row">
+        <div class="col-md-6 col-lg-3">
+            <div class="card border-top border-warning">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">To Do</h5>
+                        <h5 class="card-title">6 Orders</h5>
+                    </div>
+                    <x-todo />
+                    <x-todo />
+                    <x-todo />
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+            <div class="card border-top border-primary">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">On going Repair</h5>
+                        <h5 class="card-title">6 Orders</h5>
+                    </div>
+                    <x-todo />
+                    <x-todo />
+                    <x-todo />
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+            <div class="card border-top border-secondary">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">Testing</h5>
+                        <h5 class="card-title">6 Orders</h5>
+                    </div>
+                    <x-todo />
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3">
+            <div class="card border-top border-success">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="card-title">Completed</h5>
+                        <h5 class="card-title">6 Orders</h5>
+                    </div>
+                    <x-todo />
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
